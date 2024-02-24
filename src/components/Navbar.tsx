b38300701d86d079af14e08653ef47b8db7f80c8
Raxuis <raclot.raphael@gmail.com>
2024-02-24 14:58:31 +0100

    Feat(navbar) : Links in the navbar

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,10 +1,16 @@
 import Image from "next/image";
+import Link from "next/link";
 
 export function Navbar() {
   return (
     <div className="flex justify-between">
-      <Image src='/logo.svg' alt="logo" width={40} height={40} />
-      <p className="text-gray-300 text-xl duration-200 hover:translate-x-3">ChatBot IO</p>
+      <Link href={"/"} className="flex">
+        <Image src='/logo.svg' alt="logo" width={40} height={40} priority={true} />
+        <p className="text-gray-300 text-2xl duration-200 hover:translate-x-3 pt-2 pl-2">ChatBot IO.</p>
+      </Link>
+      <div className="flex items-center space-x-4">
+        <Link href={"/"}><p className="text-gray-300 text-2xl duration-200 hover:translate-x-3">Contact.</p></Link>
+      </div>
     </div>
   )
 }
