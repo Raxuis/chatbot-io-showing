3363d84b8e24c73533396f3ef0fddff955421947
Raxuis <raclot.raphael@gmail.com>
2024-02-25 11:39:34 +0100

    Feat(contact page) : made a contact page to contact participants

---
+++ src/app/contact/page.tsx
@@ -0,0 +1,14 @@
+import { Navbar } from '@/components/Navbar'
+import React from 'react'
+
+const Contact = () => {
+  return (
+    <div className="min-h-screen w-full">
+      <div className='w-[80%] mx-auto pt-2'>
+        <Navbar />
+      </div>
+    </div>
+  )
+}
+
+export default Contact
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,7 @@ import { Navbar } from '@/components/Navbar';
 import { BentoCustom } from '../components/ui/BentoCustom';
 export default function Home() {
   return (
-    <div className="min-h-screen w-full bg-[#0a0a0a]">
+    <div className="min-h-screen w-full">
       <div className='w-[80%] mx-auto pt-2'>
         <Navbar />
         <BentoCustom />
