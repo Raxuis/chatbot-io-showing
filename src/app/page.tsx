b253028c6937af52e4a7c25ccd3aa9d258b1c049
RaphRac <raclot.raphael@gmail.com>
2024-02-26 01:31:44 +0100

    Feat(infinite moving cards) : Implemented infinite moving cards

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,14 @@
 import { Navbar } from '@/components/Navbar';
 import { BentoCustom } from '../components/ui/BentoCustom';
+import { InfiniteMovingCardsCustom } from '@/components/ui/infiteCardsCustom';
 export default function Home() {
   return (
     <div className="min-h-screen w-full">
       <div className='w-[80%] mx-auto pt-2'>
         <Navbar />
         <BentoCustom />
+        <hr className='my-24 w-[80%] mx-auto text-grey-links' />
+        <InfiniteMovingCardsCustom />
       </div>
     </div>
   );
